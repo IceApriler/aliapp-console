10df43e1580ee415c3e93f6d652bd5a4f7455e3b
qimengjie <qimengjie@163.com>
2024-06-12 18:24:32 +0800

    chore: 编译和打包

--- build/gulp.base.js
+++ build/gulp.base.js
@@ -8,6 +8,8 @@ const child_process = require('child_process')
 const config = require('./gulp.config')
 const utils = require('./gulp.utils')
 const watch = require('gulp-watch')
+const babel = require('gulp-babel')
+const sourcemaps = require('gulp-sourcemaps')
 
 sass.compiler = require('node-sass')
 
@@ -26,7 +28,10 @@ gulp.task('compile:scss', function () {
 /** 编译scss文件 */
 gulp.task('compile:examplescss', function () {
   return gulp
-    .src([utils.makeGlob(config.exampleScssPath), '!' + utils.makeGlob(config.exampleAliappConsolePath)])
+    .src([
+      utils.makeGlob(config.exampleScssPath),
+      '!' + utils.makeGlob(config.exampleAliappConsolePath),
+    ])
     .pipe(sass().on('error', sass.logError))
     .pipe(
       rename((path) => {
@@ -39,16 +44,43 @@ gulp.task('compile:examplescss', function () {
 /** 监听scss */
 gulp.task('watch:scss', function (done) {
   watch(utils.makeGlob(config.scssPath), gulp.series('compile:scss'))
-  watch(utils.makeGlob(config.exampleScssPath), gulp.series('compile:examplescss'))
+  watch(
+    utils.makeGlob(config.exampleScssPath),
+    gulp.series('compile:examplescss'),
+  )
   done()
 })
 
 gulp.task('copy:comp', function () {
-  return gulp.src(utils.makeGlob(config.aliappConsolePath)).pipe(gulp.dest(utils.makeGlob(config.exampleAliappConsolePath)))
+  return gulp
+    .src(utils.makeGlob(config.aliappConsolePath))
+    .pipe(gulp.dest(utils.makeGlob(config.exampleAliappConsolePath)))
 })
 
 gulp.task('watch:comp', function () {
   watch(utils.makeGlob(config.aliappConsolePath), gulp.series('copy:comp'))
+})
+
+/** 编译 */
+gulp.task('build:js', function () {
+  return gulp
+    .src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        babelrc: false, // 禁用 .babelrc 或 babel.config.js 文件
+        presets: [
+          ['@babel/preset-env', { modules: false }], // 直接在 Gulp 任务中设置模块类型
+        ],
+      }),
+    )
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'))
+})
+gulp.task('build:other', function () {
+  return gulp
+    .src(['src/**/*.axml', 'src/**/*.acss', 'src/**/*.json'])
+    .pipe(gulp.dest('dist'))
 })
 
 /** 修改app.json */
@@ -94,4 +126,9 @@ function exec(cmd) {
 }
 
 /** 开发模式 */
-gulp.task('default', gulp.series('compile:scss', 'compile:examplescss', 'copy:comp', 'watch:scss', 'watch:comp'))
+gulp.task(
+  'default',
+  gulp.series('compile:scss', 'compile:examplescss', 'copy:comp'),
+)
+gulp.task('watch', gulp.series('watch:scss', 'watch:comp'))
+gulp.task('build', gulp.series('build:js', 'build:other'))
